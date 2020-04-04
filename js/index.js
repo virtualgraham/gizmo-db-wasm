860e0c4eee726a147bf88539287a10c387ba21a0
Goat <goatgoat@Goats-iMac.local>
2020-04-04 11:44:42 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -71,6 +71,6 @@
     
 
 
-// function arrays_equal(array1, array2) {
-//     return array1.length === array2.length && array1.sort().every(function(value, index) { return value === array2.sort()[index]});
-// }
+function arrays_equal(array1, array2) {
+    return array1.length === array2.length && array1.sort().every(function(value, index) { return value === array2.sort()[index]});
+}
